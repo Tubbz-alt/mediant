dc95f2b1b958362906245776615325e860b6199f
Sean Wu <seanwu1105@gmail.com>
2019-10-03 18:29:13 +0800

    Reset project to only have Main activity.

--- app/src/main/java/io/numbers/mediant/ui/main/MainActivity.kt
+++ app/src/main/java/io/numbers/mediant/ui/main/MainActivity.kt
@@ -1,25 +1,16 @@
-package io.numbers.mediant
+package io.numbers.mediant.ui.main
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import io.numbers.mediant.ui.main.SectionsPagerAdapter
+import io.numbers.mediant.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
