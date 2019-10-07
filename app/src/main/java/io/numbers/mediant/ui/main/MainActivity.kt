da348c97cd6dff59b34fdd161b8809e31a4e4140
Sean Wu <seanwu1105@gmail.com>
2019-10-07 16:45:29 +0800

    Build main activity layout to test ViewModel.

--- app/src/main/java/io/numbers/mediant/ui/main/MainActivity.kt
+++ app/src/main/java/io/numbers/mediant/ui/main/MainActivity.kt
@@ -2,8 +2,6 @@ package io.numbers.mediant.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import io.numbers.mediant.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
