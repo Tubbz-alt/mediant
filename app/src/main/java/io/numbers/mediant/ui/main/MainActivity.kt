171a07458fc26b66e19ff23ccc9e1b7278182117
Sean Wu <seanwu1105@gmail.com>
2019-10-07 17:11:47 +0800

    Use Data Binding to bind ViewModel.

--- app/src/main/java/io/numbers/mediant/ui/main/MainActivity.kt
+++ app/src/main/java/io/numbers/mediant/ui/main/MainActivity.kt
@@ -2,12 +2,19 @@ package io.numbers.mediant.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import io.numbers.mediant.R
+import io.numbers.mediant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val model = ViewModelProviders.of(this)[MainViewModel::class.java]
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.model = model
     }
 }
---
+++ app/src/main/java/io/numbers/mediant/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.numbers.mediant.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val name = "My name!"
+}
