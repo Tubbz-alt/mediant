7a5f1d6a8b7c521cc6efa612c0be000243989e0e
Sean Wu <seanwu1105@gmail.com>
2019-10-09 15:14:40 +0800

    Add Mediant logo and icon.

--- app/src/main/java/io/numbers/mediant/ui/main/InitializationViewModel.kt
+++ app/src/main/java/io/numbers/mediant/ui/main/InitializationViewModel.kt
@@ -2,15 +2,14 @@ package io.numbers.mediant.ui.main
 
 import androidx.lifecycle.ViewModel
 import io.numbers.mediant.repo.TextileRepository
-import timber.log.Timber
 
 class InitializationViewModel : ViewModel() {
 
     private val textileRepository: TextileRepository = TextileRepository()
-    var name = textileRepository.nodeOnline
-
-    fun onClick() {
-        name.value = name.value?.not()
-        Timber.d("name has changed to: ${name.value}")
-    }
+//    var name = textileRepository.nodeOnline
+//
+//    fun onClick() {
+//        name.value = name.value?.not()
+//        Timber.d("name has changed to: ${name.value}")
+//    }
 }
