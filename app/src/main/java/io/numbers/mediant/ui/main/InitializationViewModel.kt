85c7d3e6a7ae0b34d7f33197fbbe3e3d24975069
Sean Wu <seanwu1105@gmail.com>
2019-10-09 15:54:14 +0800

    Create two-way data binding for user name input text field.

--- app/src/main/java/io/numbers/mediant/ui/main/InitializationViewModel.kt
+++ app/src/main/java/io/numbers/mediant/ui/main/InitializationViewModel.kt
@@ -1,15 +1,12 @@
 package io.numbers.mediant.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.numbers.mediant.repo.TextileRepository
+import timber.log.Timber
 
 class InitializationViewModel : ViewModel() {
 
-    private val textileRepository: TextileRepository = TextileRepository()
-//    var name = textileRepository.nodeOnline
-//
-//    fun onClick() {
-//        name.value = name.value?.not()
-//        Timber.d("name has changed to: ${name.value}")
-//    }
+    val userName = MutableLiveData("")
+
+    fun onCreateAccount() = Timber.d(userName.value)
 }
