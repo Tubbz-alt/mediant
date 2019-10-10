8a8ab4bfe7789374ac26aad24ce3d7229c89e2f7
Sean Wu <seanwu1105@gmail.com>
2019-10-10 20:58:49 +0800

    Add customized ViewModelProvider.Factory for DI with ViewModels.

---
+++ app/src/main/java/io/numbers/mediant/viewmodel/ViewModelProvidersFactory.kt
@@ -0,0 +1,29 @@
+package io.numbers.mediant.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class ViewModelProvidersFactory @Inject constructor(
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+        // * if the view-model has not been created (creators[modelClass] is null)
+        // -> find the first assignable class within allowed classes with the @ViewModelKey (creators)
+        // * if not one of the assignable class is found, throw exception
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
