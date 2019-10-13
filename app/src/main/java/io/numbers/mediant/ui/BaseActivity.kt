616c71af0ba4e17e604042471a708c4326fbf8a4
Sean Wu <seanwu1105@gmail.com>
2019-10-13 23:24:45 +0800

    Apply AppBarConfiguration of navigation to the toolbar.

--- app/src/main/java/io/numbers/mediant/ui/BaseActivity.kt
+++ app/src/main/java/io/numbers/mediant/ui/BaseActivity.kt
@@ -1,8 +1,12 @@
 package io.numbers.mediant.ui
 
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 import io.numbers.mediant.R
+import kotlinx.android.synthetic.main.activity_base.*
 
 // Extends from DaggerAppCompatActivity so we do NOT need to write `AndroidInjection.inject(this)`
 // in BaseActivity.onCreate() method.
@@ -11,5 +15,9 @@ class BaseActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.initializationFragment, R.id.mainFragment))
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
